5af547db8ed7e8b64b132cbe53f4d98f888b0f17
funkydude <funkydude@users.noreply.github.com>
2015-05-14 17:13:44 +0000

    Archimonde: Rename module to prevent conflict with new Archimonde.

--- Hyjal/Archimonde.lua
+++ Hyjal/Archimonde.lua
@@ -2,7 +2,7 @@
 -- Module Declaration
 --
 
-local mod, CL = BigWigs:NewBoss("Archimonde", 775)
+local mod, CL = BigWigs:NewBoss("ArchimondeHyjal", 775)
 if not mod then return end
 mod:RegisterEnableMob(17968)
 
