50f19907b5d4e9acf768a426b6977e91bfd10040
Adavak LEE <adavak@adavak.com>
2017-05-28 12:59:41 +0800

    update zhCN. (#7)

--- BlackTemple/Locales/zhCN.lua
+++ BlackTemple/Locales/zhCN.lua
@@ -3,9 +3,9 @@ if not L then return end
 if L then
 	L.normal_phase_trigger = "苏普雷姆斯愤怒地击打着地面！"
 	L.kite_phase_trigger = "地面崩裂了！"
-	--L.normal_phase = "Normal Phase"
-	--L.kite_phase = "Kite Phase"
-	--L.next_phase = "Next Phase"
+	L.normal_phase = "一般阶段"
+	L.kite_phase = "风筝阶段"
+	L.next_phase = "下一阶段"
 end
 
 L = BigWigs:NewBossLocale("High Warlord Naj'entus", "zhCN")
@@ -16,7 +16,7 @@ end
 L = BigWigs:NewBossLocale("Reliquary of Souls", "zhCN")
 if L then
 	L.zero_mana = "零法力"
-	--L.zero_mana_desc = "Show the time it will take until the Essence of Desire has reduced everyones maximum mana to 0."
+	L.zero_mana_desc = "当欲望精华将减少所有人法力值为零时显示计时器。"
 	L.desire_start = "欲望精华！160秒后零法力。"
 end
 
