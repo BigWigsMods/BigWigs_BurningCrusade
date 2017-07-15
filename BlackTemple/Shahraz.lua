699119788246c35bb062d45f6a10d2d6eb26e32a
funkydude <funkydude@users.noreply.github.com>
2017-07-15 05:10:09 +0100

    BlackTemple/Shahraz: Add say & proximity for Fatal Attraction.

--- BlackTemple/Shahraz.lua
+++ BlackTemple/Shahraz.lua
@@ -7,7 +7,7 @@ local mod = BigWigs:NewBoss("Mother Shahraz", 796, 1588)
 if not mod then return end
 mod:RegisterEnableMob(22947)
 mod.engageId = 607
---mod.respawnTime = 0
+--mod.respawnTime = 0 -- Resets, doesn't respawn
 
 --------------------------------------------------------------------------------
 -- Locals
@@ -21,7 +21,7 @@ local playerList = mod:NewTargetList()
 
 function mod:GetOptions()
 	return {
-		41001, -- Fatal Attraction
+		{41001, "SAY", "PROXIMITY"}, -- Fatal Attraction
 		40883, -- Prismatic Aura: Nature
 		40891, -- Prismatic Aura: Arcane
 		40880, -- Prismatic Aura: Shadow
@@ -34,6 +34,7 @@ end
 
 function mod:OnBossEnable()
 	self:Log("SPELL_AURA_APPLIED", "FatalAttraction", 41001)
+	self:Log("SPELL_AURA_REMOVED", "FatalAttractionRemoved", 41001)
 
 	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", nil, "boss1")
 end
@@ -49,8 +50,20 @@ end
 
 function mod:FatalAttraction(args)
 	playerList[#playerList+1] = args.destName
+
+	if self:Me(args.destGUID) then
+		self:Say(args.spellId)
+		self:OpenProximity(args.spellId, 15)
+	end
+
 	if #playerList == 1 then
 		self:ScheduleTimer("TargetMessage", 0.3, args.spellId, playerList, "Urgent", "Alert")
+	end
+end
+
+function mod:FatalAttractionRemoved(args)
+	if self:Me(args.destGUID) then
+		self:CloseProximity(args.spellId)
 	end
 end
 
