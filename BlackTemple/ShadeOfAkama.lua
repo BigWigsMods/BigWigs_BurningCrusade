5d9cf0bbce7b2f72fe4ea5079a16fe8f270de83d
funkydude <funkydude@users.noreply.github.com>
2026-08-29 01:10:59 +0100

    BlackTemple/ShadeOfAkama: Cleanups

--- BlackTemple/ShadeOfAkama.lua
+++ BlackTemple/ShadeOfAkama.lua
@@ -8,6 +8,7 @@ if not mod then return end
 mod:RegisterEnableMob(23191, 22841) -- Akama, Shade of Akama
 mod:SetEncounterID(603)
 mod:SetRespawnTime(120)
+mod:SetStage(1)
 
 --------------------------------------------------------------------------------
 -- Locals
@@ -42,102 +43,128 @@ function mod:GetOptions()
 end
 
 function mod:VerifyEnable(unit, mobId)
-	if self:Classic() then
-		return false -- XXX fixme on classic
-	end
 	if mobId == 22841 or self:GetHealth(unit) == 100 then -- Enable if shade, or if Akama at 100% hp
 		return true
 	end
 end
 
 function mod:OnBossEnable()
-	if not self:Classic() then
-		self:Log("SPELL_AURA_REMOVED", "StealthRemoved", 34189)
-		self:RegisterMessage("BigWigs_BossComm")
+	self:Log("SPELL_AURA_REMOVED", "StealthRemoved", 34189)
+	self:RegisterMessage("BigWigs_BossComm")
 
-		self:Log("SPELL_AURA_APPLIED", "RainOfFireDamage", 42023)
-		self:Log("SPELL_PERIODIC_DAMAGE", "RainOfFireDamage", 42023)
-		self:Log("SPELL_PERIODIC_MISSED", "RainOfFireDamage", 42023)
+	self:Log("SPELL_AURA_APPLIED", "RainOfFireDamage", 42023)
+	self:Log("SPELL_PERIODIC_DAMAGE", "RainOfFireDamage", 42023)
+	self:Log("SPELL_PERIODIC_MISSED", "RainOfFireDamage", 42023)
 
+	self:Log("SWING_DAMAGE", "SwingDamage", "*")
+
+	if self:Classic() then
 		self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
 	end
 end
 
 function mod:OnEngage()
-	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT")
-	self:MessageOld("stages", "cyan", "info", L.engaged, false)
-	self:Bar("stages", 13, L.defender, 159241) -- ability_parry / icon 132269
-	self:ScheduleTimer("Bar", 13, "stages", 32, L.defender, 159241)
-	defender = self:ScheduleTimer("RepeaterDefender", 45)
-	self:Bar("stages", 13, L.sorcerer, 193473) -- spell_shadow_siphonmana / icon 136208
-	sorcerer = self:ScheduleTimer("RepeaterSorcerer", 13)
-	self:Bar("stages", 13, L.adds_right, 87219) -- misc_arrowright / icon 450908
-	right = self:ScheduleTimer("RepeaterAddsRight", 13)
-	self:Bar("stages", 28, L.adds_left, 87217) -- misc_arrowleft / icon 450906
-	left = self:ScheduleTimer("RepeaterAddsLeft", 28)
+	self:SetStage(1)
+	if not self:Classic() then
+		self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT")
+	end
+	self:Message("stages", "cyan", L.engaged, false)
+	self:Bar("stages", 32, L.defender, "ability_parry")
+	self:RepeaterSorcerer()
+	self:RepeaterAddsRight()
+	left = self:ScheduleTimer("RepeaterAddsLeft", 15)
+	defender = self:ScheduleTimer("RepeaterDefender", 32)
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
 
-function mod:StealthRemoved(args)
-	-- Stealth is removed when you speak to him, starting the encounter
-	if self:MobId(args.destGUID) == 23191 then -- Akama
-		self:Sync("Akama") -- There seems to be range problems with this, sync for now.
+do
+	local prev = 0
+	function mod:StealthRemoved(args)
+		-- Stealth is removed when you speak to him, starting the encounter
+		if self:MobId(args.destGUID) == 23191 and not self:IsEngaged() and (GetTime() - prev > 2) then -- Akama
+			self:Sync("Akama") -- There seems to be range problems with this, sync for now.
+		end
 	end
 end
 
-function mod:BigWigs_BossComm(_, msg)
-	if msg == "Akama" and not self:IsEngaged() then
-		self:Engage()
+do
+	local prev = 0
+	function mod:BigWigs_BossComm(_, msg)
+		if msg == "Akama" and not self:IsEngaged() and (GetTime() - prev > 2) then
+			prev = GetTime()
+			self:Bar("stages", 13, L.defender, "ability_parry")
+			self:Bar("stages", 13, L.sorcerer, "spell_shadow_siphonmana")
+			self:Bar("stages", 13, L.adds_right, "misc_arrowright")
+			self:Bar("stages", 28, L.adds_left, "misc_arrowleft")
+		end
 	end
 end
 
 function mod:RepeaterDefender()
-	self:Bar("stages", 29.7, L.defender, 159241) -- ability_parry / icon 132269
+	self:Bar("stages", 29.7, L.defender, "ability_parry")
 	defender = self:ScheduleTimer("RepeaterDefender", 29.7)
 end
 
 function mod:RepeaterSorcerer()
-	self:Bar("stages", 25.5, L.sorcerer, 193473) -- spell_shadow_siphonmana / icon 136208
+	self:Bar("stages", 25.5, L.sorcerer, "spell_shadow_siphonmana")
 	sorcerer = self:ScheduleTimer("RepeaterSorcerer", 25.5)
 end
 
 function mod:RepeaterAddsRight()
-	self:Bar("stages", 45, L.adds_right, 87219) -- misc_arrowright / icon 450908
+	self:Bar("stages", 45, L.adds_right, "misc_arrowright")
 	right = self:ScheduleTimer("RepeaterAddsRight", 45)
 end
 
 function mod:RepeaterAddsLeft()
-	self:Bar("stages", 52, L.adds_left, 87217) -- misc_arrowleft / icon 450906
+	self:Bar("stages", 52, L.adds_left, "misc_arrowleft")
 	left = self:ScheduleTimer("RepeaterAddsLeft", 52)
 end
 
 do
 	local prev = 0
 	function mod:RainOfFireDamage(args)
-		local t = GetTime()
-		if self:Me(args.destGUID) and t-prev > 1.5 then
-			prev = t
-			self:MessageOld(args.spellId, "blue", "alert", CL.you:format(args.spellName))
+		if self:Me(args.destGUID) and args.time - prev > 2 then
+			prev = args.time
+			self:PersonalMessage(args.spellId, "aboveyou")
+			self:PlaySound(args.spellId, "underyou")
 		end
 	end
 end
 
-function mod:INSTANCE_ENCOUNTER_ENGAGE_UNIT()
-	if self:MobId(self:UnitGUID("boss1")) == 22841 then -- Shade of Akama
-		self:CancelTimer(defender)
-		self:CancelTimer(sorcerer)
-		self:CancelTimer(right)
-		self:CancelTimer(left)
-		defender, sorcerer, left, right = nil, nil, nil, nil
-		self:UnregisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT")
-		self:StopBar(L.defender)
-		self:StopBar(L.sorcerer)
-		self:StopBar(L.adds_right)
-		self:StopBar(L.adds_left)
-		self:MessageOld("stages", "cyan", "info", CL.stage:format(2), false)
+do
+	local function GoStage2(self)
+		if self:GetStage() == 1 then
+			self:SetStage(2)
+			self:CancelTimer(defender)
+			self:CancelTimer(sorcerer)
+			self:CancelTimer(right)
+			self:CancelTimer(left)
+			defender, sorcerer, left, right = nil, nil, nil, nil
+			self:StopBar(L.defender)
+			self:StopBar(L.sorcerer)
+			self:StopBar(L.adds_right)
+			self:StopBar(L.adds_left)
+			self:Message("stages", "cyan", CL.stage:format(2), false)
+			self:PlaySound("stages", "info")
+		end
+	end
+
+	function mod:SwingDamage(args) -- Classic
+		local destCreatureID = self:MobId(args.destGUID)
+		if destCreatureID == 23191 or destCreatureID == 22841 then -- Akama, Shade of Akama
+			self:RemoveLog("SWING_DAMAGE", "*")
+			GoStage2(self)
+		end
+	end
+
+	function mod:INSTANCE_ENCOUNTER_ENGAGE_UNIT() -- Retail
+		if UnitExists("boss1") then
+			self:UnregisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT")
+			GoStage2(self)
+		end
 	end
 end
 
